e24f6113347cbbfd004e272ddfe246dd75c3b91b
DoMinhHieu99 <dohieu19999@gmail.com>
2019-08-29 10:01:01 +0700

    test1

--- app/src/main/java/dohieu/com/myapplication/Main2Activity.java
+++ app/src/main/java/dohieu/com/myapplication/Main2Activity.java
@@ -3,6 +3,7 @@ package dohieu.com.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
 }
